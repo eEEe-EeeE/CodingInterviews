8b14205d3578c81c09d0b563430a5d376c76bc87
BPanther <civilian_x@163.com>
2020-01-02 21:04:50 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,20 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(-11 % -3);
     }
 
+    static int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            throw new ArithmeticException("divisor is zero.");
+        if (dividend == 0)
+            return 0;
+        boolean signIdentical;
+        boolean dividendPositive;
+        signIdentical = (dividend & Integer.MIN_VALUE) == (divisor & Integer.MIN_VALUE);
+        dividendPositive = (dividend & Integer.MIN_VALUE) == 0;
+
+        return 0;
+    }
 
 }
